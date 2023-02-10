361d3f634f94692bf3e0c92357cf4b161b86c465
Sabir Hassan <sabirbk06@gmail.com>
2023-02-10 23:20:43 +0500

    quick fix

--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -456,7 +456,11 @@ export const Room = () => {
                 <path d='M0 1v17h24v-17h-24zm22 15h-20v-13h20v13zm-6.599 4l2.599 3h-12l2.599-3h6.802z' />
               </svg>
             </button>
-            <button id='leave-btn' style={{ backgroundColor: '#FF5050' }}>
+            <button
+              id='leave-btn'
+              style={{ backgroundColor: '#FF5050' }}
+              onClick={leaveStream}
+            >
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 width='24'
